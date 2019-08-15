ea61e67a11ea66321b922676e294e00352823252
Tiffany <Tafby88@gmail.com>
2019-08-15 11:14:20 -0700

    created movements for trees and rocks

--- src/Grid.js
+++ src/Grid.js
@@ -7,19 +7,19 @@ export default class Grid extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			row: 1,
-			column: 1,
-			moveRow: 0,
-			moveColumn: 0,
-			treeCoords: [ [ Math.floor(Math.random() * 30) + 1, Math.floor(Math.random() * 30) + 1 ] ],
+			y: 1,
+			x: 1,
+			moveY: 0,
+			moveX: 0,
+			treeCoords: [ [ 19, 19 ] ],
 			rockCoords: [ [ 2, 5 ] ],
-			direction: 'ArrowRight'
+			direction: null
 		};
 	}
 
 	componentDidMount = () => {
 		document.addEventListener('keydown', this.keyListener);
-		this.intervalID = window.setInterval(this.gameLoop, 100);
+		this.intervalID = window.setInterval(this.gameLoop, 200);
 		this.generateTrees();
 		this.generateRocks();
 	};
@@ -31,39 +31,74 @@ export default class Grid extends Component {
 	keyListener = (event) => {
 		switch (event.key) {
 			case 'ArrowLeft':
-				this.setState({ moveColumn: -1, moveRow: 1, nextDirection: 'left' });
+				this.setState({ moveX: -1, moveY: 1, direction: 'left' });
 				break;
 			case 'ArrowRight':
-				this.setState({ moveColumn: 1, moveRow: 1, nextDirection: 'right' });
+				this.setState({ moveX: 1, moveY: 1, direction: 'right' });
 				break;
 			case 'ArrowDown':
 				if (this.state.direction === 'up') {
 					break;
 				}
-				this.setState({ moveColumn: 0, moveRow: 1, nextDirection: 'down' });
+				this.setState({ moveX: 0, moveY: 1, direction: 'down' });
 				break;
 			case 'ArrowUp':
-				this.setState({ moveColumn: 0, moveRow: -1, nextDirection: 'up' });
+				this.setState({ moveX: 0, moveY: -1, direction: 'up' });
 				break;
 		}
 	};
 
 	move = () => {
-		this.setState({ row: this.state.row + this.state.moveRow, column: this.state.column + this.state.moveColumn });
-		if (this.state.row === 30) {
-			this.generateRocks();
-			this.generateTrees();
-		}
+		//MOVE TREES
+		let newtreearray = [];
+		this.state.treeCoords.map((tree) => {
+			let treeCopy = [ tree[0], tree[1] ];
+			if (this.state.direction === 'left') {
+				// newtreearray.push([ tree[0] + 1, tree[1] + 1 ]);
+				treeCopy[0] += 1;
+				treeCopy[1] += 1;
+			} else if (this.state.direction === 'right') {
+				// newtreearray.push([ tree[0] - 1, tree[1] + 1 ]);
+				treeCopy[0] -= 1;
+				treeCopy[1] += 1;
+			} else if (this.state.direction === 'down') {
+				treeCopy[0] -= 1;
+			}
+			newtreearray.push(treeCopy);
+		});
+		//END MOVE TREES
+		//MOVE ROCKS
+		let newrockarray = [];
+		this.state.rockCoords.map((rock) => {
+			let rockCopy = [ rock[0], rock[1] ];
+			if (this.state.direction === 'left') {
+				// newrockarray.push([ rock[0] + 1, rock[1] + 1 ]);
+				rockCopy[0] += 1;
+				rockCopy[1] += 1;
+			} else if (this.state.direction === 'right') {
+				// newrockarray.push([ rock[0] - 1, rock[1] + 1 ]);
+				rockCopy[0] -= 1;
+				rockCopy[1] += 1;
+			} else if (this.state.direction === 'down') {
+				rockCopy[0] -= 1;
+			}
+			newrockarray.push(rockCopy);
+		});
+		//END MOVE ROCKS
+
+		this.setState({
+			treeCoords: newtreearray,
+			rockCoords: newrockarray,
+			y: this.state.y + this.state.moveY,
+			x: this.state.x + this.state.moveX
+		});
 	};
 
 	generateTrees = () => {
-		let newRow = Math.floor(Math.random() * 50) + 1;
-		let newCol = Math.floor(Math.random() * 50) + 1;
-		let treeArray = [ [ newRow, newCol ] ];
-		this.setState({ treeCoords: [ newRow, newCol ] });
+		let treeArray = [];
 		for (let i = 0; i < 15; i++) {
-			newRow = Math.floor(Math.random() * 50) + 1;
-			newCol = Math.floor(Math.random() * 50) + 1;
+			let newRow = Math.floor(Math.random() * 50) + 1;
+			let newCol = Math.floor(Math.random() * 50) + 1;
 			console.log('tree', newRow, newCol);
 			treeArray.push([ newRow, newCol ]);
 		}
@@ -88,9 +123,9 @@ export default class Grid extends Component {
 	render() {
 		return (
 			<div className="grid">
-				<Skier row={this.state.row} column={this.state.column} />
-				{this.state.treeCoords.map((tree) => {
-					return <Tree row={tree[0]} col={tree[1]} />;
+				<Skier row={this.state.y} column={this.state.x} />
+				{this.state.treeCoords.map((tree, index) => {
+					return <Tree row={tree[0]} col={tree[1]} key={index} />;
 				})}
 				{this.state.rockCoords.map((rock) => {
 					return <Rock row={rock[0]} col={rock[1]} />;
